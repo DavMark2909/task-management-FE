cf45e37e02c1e65024dff38fcea9b127ac741e52
DavMark2909 <77192509+DavMark2909@users.noreply.github.com>
2024-07-18 20:37:25 -0400

    adding dialog component without imperial handling

--- app_management_fe/src/components/DialogComponent.js
+++ app_management_fe/src/components/DialogComponent.js
@@ -1,28 +1,47 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./css/Dialog.module.css";
 import Message from "./Message";
 
 function DialogComponent({messages, username, onAdd}){
 
     const inputRef = useRef();
+    const listRef = useRef();
 
     const parsed = messages.map(message => ({
         ...message,
         side: message.sender === username
     }));   
 
-    function addMessage(){
+    const [stateMsg, setStateMsg] = useState(parsed);
+
+    const scrollToButton = () => {
+        if (listRef.current){
+            listRef.current.scrollTop = listRef.current.scrollHeight;
+        }
+    }  
+
+    function addUserMessage(){
         const message = inputRef.current.value;
+        addMessage(message, true);
+        inputRef.current.value = "";
+    }
+
+    function addMessage(message, type = false){
         if (message.trim() !== ""){
-            onAdd(message)
-            inputRef.current.value = "";  
+            onAdd(message);
+            const newMsg = {
+                content: message,
+                side: type
+            };
+            setStateMsg([...setStateMsg, newMsg]);
+            scrollToButton();  
         }
     }
 
     return (
         <div className={classes.main}>
-            <ul className={classes.dialog}>
-                {parsed.map((content, index) => (
+            <ul className={classes.dialog} ref={listRef}>
+                {stateMsg.map((content, index) => (
                     <li key={index} className={content.side ? classes.mymessage : classes.sidemessage}>
                         <Message text={content.content} type={content.side}/>
                     </li>
@@ -30,7 +49,7 @@ function DialogComponent({messages, username, onAdd}){
             </ul>
             <div className={classes.messagepane}>
                 <input placeholder="Type message" ref={inputRef}/>
-                <button onClick={addMessage}>Add</button>
+                <button onClick={addUserMessage}>Add</button>
             </div>
         </div> 
     );
